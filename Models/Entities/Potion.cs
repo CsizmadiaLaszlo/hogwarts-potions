1ca6eef87f093253f0bb94bddd1423b2a5479a73
Csizmadia László <csizilaci92@gmail.com>
2022-10-02 14:24:58 +0200

    Add potion model

---
+++ Models/Entities/Potion.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+using HogwartsPotions.Models.Enums;
+
+namespace HogwartsPotions.Models.Entities;
+
+public class Potion
+{
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public long Id { get; set; }
+    [Required]
+    public Student Brewer { get; set; }
+    
+    public string Name => $"{Brewer.Name}'s Potions";
+    
+    public HashSet<Ingredient> Ingredients { get; set; } = new();
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public BrewingStatus BrewingStatus { get; set; } = BrewingStatus.Brew;
+    public Recipe Recipe { get; set; }
+}
