8589938e43a98c1af9444e74d8b9c61de703fc40
Csizmadia László <csizilaci92@gmail.com>
2022-10-02 14:43:05 +0200

    Create PotionController.cs

---
+++ Controllers/PotionController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using HogwartsPotions.Models.Entities;
+using HogwartsPotions.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HogwartsPotions.Controllers
+{
+    [ApiController, Route("/potions")]
+    public class PotionController : ControllerBase
+    {
+        private readonly IPotionService _potionService;
+
+        public PotionController(IPotionService potionService)
+        {
+            _potionService = potionService;
+        }
+
+    }
+}
