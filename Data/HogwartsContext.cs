5228fe88bb2548e4b118ecdc13a2824d05261c5a
Csizmadia László <csizilaci92@gmail.com>
2022-10-02 14:28:20 +0200

    Add context

---
+++ Data/HogwartsContext.cs
@@ -0,0 +1,27 @@
+using HogwartsPotions.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HogwartsPotions.Data
+{
+    public class HogwartsContext : DbContext
+    {
+        public HogwartsContext(DbContextOptions<HogwartsContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Recipe> Recipes { get; set; }
+        public DbSet<Ingredient> Ingredients { get; set; }
+        public DbSet<Potion> Potions { get; set; }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>().ToTable("Student");
+            modelBuilder.Entity<Room>().ToTable("Room");
+            modelBuilder.Entity<Ingredient>().ToTable("Ingredient");
+            modelBuilder.Entity<Recipe>().ToTable("Recipe");
+            modelBuilder.Entity<Potion>().ToTable("Potion");
+        }
+    }
+}
