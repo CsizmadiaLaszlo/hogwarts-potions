19a98319dd2eda7b9f99b121f5df54da789414b2
Csizmadia László <csizilaci92@gmail.com>
2022-10-02 14:25:40 +0200

    Add student model

---
+++ Models/Entities/Student.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+using HogwartsPotions.Models.Enums;
+
+namespace HogwartsPotions.Models.Entities
+{
+    public class Student
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public HouseType HouseType { get; set; }
+
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public PetType PetType { get; set; }
+
+        [JsonIgnore] public Room Room { get; set; }
+    }
+}
