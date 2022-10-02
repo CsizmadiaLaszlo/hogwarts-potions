ec6706039994794d3ab7687e069c3e582d57c228
Csizmadia László <csizilaci92@gmail.com>
2022-10-02 14:52:14 +0200

    Create RoomService.cs

---
+++ Services/RoomService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HogwartsPotions.Data;
+using HogwartsPotions.Models.Entities;
+using HogwartsPotions.Models.Enums;
+using HogwartsPotions.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace HogwartsPotions.Services;
+
+public class RoomService : IRoomService
+{
+    private readonly HogwartsContext _context;
+
+    public RoomService(HogwartsContext context)
+    {
+        _context = context;
+    }
+
+    }
+}
