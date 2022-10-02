63034582e77407e35a7ba7409b0963fe4fb3f8a0
Csizmadia László <csizilaci92@gmail.com>
2022-10-02 14:51:37 +0200

    Create IRoomService.cs

---
+++ Services/Interfaces/IRoomService.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using HogwartsPotions.Models.Entities;
+
+namespace HogwartsPotions.Services.Interfaces;
+
+public interface IRoomService
+{
+    Task AddRoom(Room room);
+    Task<Room> GetRoom(long id);
+    Task DeleteRoom(long id);
+    Task UpdateRoom(Room room);
+    Task<List<Room>> GetAllRooms();
+    Task<List<Room>> GetRoomsForRatOwners();
+    Task<List<Room>> GetAvailableRooms();
+}
