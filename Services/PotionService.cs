dacd3e3a3b869bc61369ec4f2209fb9efe2fd7c8
Csizmadia László <csizilaci92@gmail.com>
2022-10-02 14:33:14 +0200

    Add PotionService.cs

---
+++ Services/PotionService.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HogwartsPotions.Data;
+using HogwartsPotions.Models.Entities;
+using HogwartsPotions.Models.Enums;
+using HogwartsPotions.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace HogwartsPotions.Services;
+
+public class PotionService : IPotionService
+{
+    private const int MaxIngredientsForPotions = 5;
+    private readonly HogwartsContext _context;
+
+    public PotionService(HogwartsContext context)
+    {
+        _context = context;
+    }
+
+}
