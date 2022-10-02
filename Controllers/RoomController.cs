f9befb713a701a20e7405c216656bbd777c9b3d6
Csizmadia László <csizilaci92@gmail.com>
2022-10-02 14:55:07 +0200

    Create RoomController.cs

---
+++ Controllers/RoomController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using HogwartsPotions.Models.Entities;
+using HogwartsPotions.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HogwartsPotions.Controllers
+{
+    [ApiController, Route("/room")]
+    public class RoomController : ControllerBase
+    {
+        private readonly IRoomService _roomService;
+
+        public RoomController(IRoomService roomService)
+        {
+            _roomService = roomService;
+        }
+
+    }
+}
