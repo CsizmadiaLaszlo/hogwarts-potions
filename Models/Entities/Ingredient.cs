6c280ebcd44f670b3896466ba06c447c84b78ccc
Csizmadia László <csizilaci92@gmail.com>
2022-10-02 14:26:57 +0200

    Add ingredient model

---
+++ Models/Entities/Ingredient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace HogwartsPotions.Models.Entities
+{
+    public class Ingredient
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+        [JsonIgnore]
+        public HashSet<Potion> Potions { get; set; }
+        [JsonIgnore]
+        public HashSet<Recipe> Recipes { get; set; }
+    }
+}
