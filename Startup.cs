7900fca1e833e27adb4579633a7be98c6b72f971
Csizmadia László <csizilaci92@gmail.com>
2022-10-02 14:31:16 +0200

    Add microsoft.core to database context

--- Startup.cs
+++ Startup.cs
@@ -23,6 +23,9 @@ namespace HogwartsPotions
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<HogwartsContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            
 
         }
 
